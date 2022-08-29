10bdce2af3a594aba05a4830ba0d78691a3d2a17
Yarik Hadion <110093395+YarHadi@users.noreply.github.com>
2022-08-29 09:11:46 +0300

    1

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ var Enemy = function (x, y) {
 // Parameter: dt, a time delta between ticks
 
 Enemy.prototype.update = function (dt) {
+    
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
